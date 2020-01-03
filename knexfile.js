8252c3518f627b2c64bebc9dab749055450d1dc0
Danielle B <danibaxx@hotmail.com>
2020-01-03 14:56:48 -0700

    failed to commit earlier

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,10 @@
-// Update with your config settings.
-
 module.exports = {
 
   development: {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
+      filename: '/data/car-dealer.db3'
     }
   },
 };
